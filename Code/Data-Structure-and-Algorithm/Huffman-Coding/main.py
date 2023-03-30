4d9dbe900aaaaaa65aa77168d1e399ee3307f1d5
Garvit Joshi <90491911+Garvitjoshi1@users.noreply.github.com>
2023-03-30 20:52:11 +0530

    Add files via upload

---
+++ Code/Data-Structure-and-Algorithm/Huffman-Coding/main.py
@@ -0,0 +1,25 @@
+class Huffman_Coding:
+    def __init__(self, path):
+        self.path = path
+
+    def __make_frequency_dict(self, text):
+        pass
+
+    def compress(self):
+        # get file from path
+        # read text from file
+        text = "lkjhgfdsa"
+        # make frequency dictionary using the text
+
+        # Construct the heap from the frequency_dict
+
+        # Construct the code from binary tree
+
+        # Creating the encoded text using the codes
+
+        # put this encoded text into the binary tree
+
+        # return this binary tree into binary file
+
+        pass
+
